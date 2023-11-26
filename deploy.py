c185ced6b585b5ea58bae934e5a81566d4ea22cf
Nusa <91360337+Nusa186@users.noreply.github.com>
2023-11-26 14:45:22 +0700

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -2,25 +2,28 @@ import PIL
 from PIL import Image
 from flask import Flask, request, jsonify
 import tensorflow as tf
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
-from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import load_img, img_to_array
+from werkzeug.utils import secure_filename
+from io import BytesIO
+import requests
 import numpy as np
 
 app = Flask(__name__)
 model = tf.keras.models.load_model('image_classifier.h5')
 
-class_indices = np.load('class_dataset.npy', allow_pickle=True).item()
+app.config["ALLOWED_EXTENSIONS"] = set(['jpg', 'png', 'jpeg'])
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in app.config["ALLOWED_EXTENSIONS"]
+
+class_indices = np.load('class_dataset.npy', allow_pickle=True).item()
 class_names = {v: k for k, v in class_indices.items()}
 
-@app.route('/')
-def index():
-    return 'Hello, world'
-    
 @app.route('/predict-single-url', methods=['POST'])
 def predict_single_url():
     img_url = request.form.get('imgUrl')
-
+    
     if img_url:
         # Mengambil gambar dari URL
         response = requests.get(img_url)
@@ -47,12 +50,13 @@ def predict_single_url():
     else:
         return jsonify({'status': 'ERROR', 'message': 'No URL provided.'}), 400
         
-@app.route('/predict-single', methods=['GET', 'POST'])
+@app.route('/predict-single', methods=['POST'])
 def predict_single():
     imgFile = request.files.get('imgFile[]')
 
-    if imgFile:
-        img_path = "./db/" + imgFile.filename
+    if imgFile and allowed_file(imgFile.filename):
+        filename = secure_filename(imgFile.filename)    
+        img_path = "./db/" + filename
         imgFile.save(img_path)
 
         img = load_img(img_path, target_size=(150, 150))
@@ -75,38 +79,40 @@ def predict_single():
         return jsonify({'status': 'SUCCESS', 'result': result}), 200
     else:
         return jsonify({'status': 'ERROR', 'message': 'No file provided.'}), 400
-
-@app.route('/predict-multiple', methods=['GET', 'POST'])
-def predict_multiple():
+    
+@app.route('/predict-multiple', methods=['POST'])
+def predict():
     files = request.files.getlist('imgFile[]')  # Menggunakan getlist untuk mengambil daftar file
 
     predictions = []
+    if files:
+        for imgFile in files:
+            if imgFile and allowed_file(imgFile.filename):
+                filename = secure_filename(imgFile.filename)    
+                img_path = "./db/" + filename
+                imgFile.save(img_path)
 
-    for imgFile in files:
-        img_path = "./db/" + imgFile.filename
-        imgFile.save(img_path)  # Simpan file ke disk
+                img = load_img(img_path, target_size=(150, 150))
 
-        img = load_img(img_path, target_size=(150, 150))
+                x = img_to_array(img) / 255.
+                x = np.expand_dims(x, axis=0)
 
-        x = img_to_array(img) / 255.
-        x = np.expand_dims(x, axis=0)
+                prediction = model.predict(x)
 
-        prediction = model.predict(x)
+                top_class = np.argmax(prediction)
+                top_prob = prediction[0][top_class]
+                
+                top_class_name = class_names[top_class]
 
-        top_class = np.argmax(prediction)
-        top_prob = prediction[0][top_class]
-        
-        top_class_name = class_names[top_class]
+                predictions.append({
+                    'input': imgFile.filename,
+                    'category': top_class_name,
+                    'prediction': f'{top_prob * 100:.2f}'
+                })
 
-        predictions.append({
-            'input': imgFile.filename,
-            'category': top_class_name,
-            'prediction': f'{top_prob * 100:.2f}'
-        })
-
-    if imgFile:
         return jsonify({'status': 'SUCCESS', 'predictions': predictions}), 200  # Format JSON untuk respons
     else:
         return jsonify({'status': 'ERROR', 'message': 'No file provided.'}), 400
+    
 if __name__ == '__main__':
     app.run(debug=True)
